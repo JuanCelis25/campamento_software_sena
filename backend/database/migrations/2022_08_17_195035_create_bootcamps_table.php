b0d9ce2ec5e13a6baa4776f40c18bfe3630f15db
JuanCelis25 <104436922+JuanCelis25@users.noreply.github.com>
2022-08-17 15:19:24 -0500

    migrations

---
+++ backend/database/migrations/2022_08_17_195035_create_bootcamps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bootcamps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('website');
+            $table->string('phone');
+            $table->float('average_rating', 8, 2);
+            $table->decimal('average_cost', $precision = 8, $scale = 2);
+            $table->foreignId('bootcamp_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bootcamps');
+    }
+};
